df0c39a4fe4f94880285f35e081c9d16b44edddb
Benjamin Landers <benjaminrlanders@gmail.com>
2016-07-20 03:57:30 -0700

    Removed Pixel class, optimized display, added lets

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ function generate() {
 	var width = 300;
 	canvas.height = height;
 	canvas.width = width;
-	for(i = 0; i < height; i++) {
+	for(let i = 0; i < height; i++) {
 		output.push([]);
-		for(j = 0; j < width; j++) {
+		for(let j = 0; j < width; j++) {
 			var num = Math.random();
 			if(num < 1 / 2)
-				output[i].push(new Pixel(j + 1, i + 1, 0));
+				output[i].push(0);
 			else if(num < 3 / 4)
-				output[i].push(new Pixel(j + 1, i + 1, 1));
+				output[i].push(1);
 			else
-				output[i].push(new Pixel(j + 1, i + 1, 2));
+				output[i].push(2);
 		}
 	}
 	allPictures.push(new Picture(output));
 	display(0);
-	for(i = 1; i <= progress.max; i++) {
+	for(let i = 1; i <= progress.max; i++) {
 		allPictures.push(allPictures[i - 1].clone());
 		progressImage(i,canvas.width,canvas.height);
 	}
@@ -55,7 +55,7 @@ function evolve() {
 	document.body.getBoundingClientRect();
 	allPictures = [allPictures[allPictures.length - 1].clone()];
 	display(0);
-	for(i = 1; i <= progress.max; i++) {
+	for(let i = 1; i <= progress.max; i++) {
 		allPictures.push(allPictures[i - 1].clone());
 		progressImage(i,canvas.width,canvas.height);
 	}
@@ -90,31 +90,31 @@ function changeState() {
 function progressImage(num, width, height) {
 	var image = allPictures[num].image;
 	var prev = allPictures[num - 1].image;
-	for(j = 0; j < image.length; j++) {
-		for(k = 0; k < image[j].length; k++) {
+	for(let j = 0; j < image.length; j++) {
+		for(let k = 0; k < image[j].length; k++) {
 			var neighbors = [];
-			neighbors.push(prev[(j-1+height)%height][(k+1+width)%width].identity);
-			neighbors.push(prev[j][(k+1)%width].identity);
-			neighbors.push(prev[(j+1)%height][(k+1)%width].identity);
-			neighbors.push(prev[(j+1)%height][k].identity);
-			neighbors.push(prev[(j+1)%height][(k-1+width)%width].identity);
-			neighbors.push(prev[j][(k-1+width)%width].identity);
-			neighbors.push(prev[(j-1+height)%height][(k-1+width)%width].identity);
-			neighbors.push(prev[(j-1+height)%height][k].identity);
-			var identity = image[j][k].identity - 1;
+			neighbors.push(prev[(j-1+height)%height][(k+1+width)%width]);
+			neighbors.push(prev[j][(k+1)%width]);
+			neighbors.push(prev[(j+1)%height][(k+1)%width]);
+			neighbors.push(prev[(j+1)%height][k]);
+			neighbors.push(prev[(j+1)%height][(k-1+width)%width]);
+			neighbors.push(prev[j][(k-1+width)%width]);
+			neighbors.push(prev[(j-1+height)%height][(k-1+width)%width]);
+			neighbors.push(prev[(j-1+height)%height][k]);
+			var identity = image[j][k] - 1;
 			var neighborCount = [];
 			neighborCount.push(countArray(neighbors,1));
 			neighborCount.push(countArray(neighbors,2));
 			if(identity != -1) {
 				if(neighborCount[identity] < 2 || neighborCount[identity] > 3)
-					image[j][k].identity = 0;
+					image[j][k] = 0;
 				if(neighborCount[identity] < neighborCount[(identity+1)%2])
-					image[j][k].identity = 0;
+					image[j][k] = 0;
 			} else {
 				if(neighborCount[0] > 2 && neighborCount[1] < 2)
-					image[j][k].identity = 1;
+					image[j][k] = 1;
 				else if(neighborCount[1] > 2 && neighborCount[0] < 2)
-					image[j][k].identity = 2;
+					image[j][k] = 2;
 			}
 		}
 	}
@@ -124,36 +124,20 @@ function display(num) {
 	ctx.fillStyle = "#FFF";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 	var image = allPictures[num].image;
-	for(i = 0; i < image.length; i++) {
-		for(j = 0; j < image[i].length; j++) {
-			image[i][j].display();
+	for(let i = 0; i < image.length; i++) {
+		for(let j = 0; j < image[i].length; j++) {
+    			ctx.fillStyle = "#" + (image[i][j] === 0 ? "FFF" : (image[i][j] == 1 ? "0F0" : "00F"));
+    			ctx.fillRect(i,j,1,1);
 		}
 	}
 }
 
 function countArray(arr, item) {
 	var count = 0;
-	for(index = 0; index < arr.length; index++)
+	for(let index = 0; index < arr.length; index++)
 		if(arr[index] == item)
 			count++;
 	return count;
-}
-
-function Pixel(x,y,identity) {
-	this.x = x;
-	this.y = y;
-	this.identity = identity;
-}
-
-Pixel.prototype.display = function() {
-	ctx.beginPath();
-	ctx.fillStyle = "#" + (this.identity === 0 ? "FFF" : (this.identity == 1 ? "0F0" : "00F"));
-	ctx.arc(this.x,this.y,1,0,2*Math.PI);
-	ctx.fill();
-}
-
-Pixel.prototype.clone= function() {
-	return new Pixel(this.x,this.y,this.identity);
 }
 
 function Picture(arrPixels) {
@@ -162,10 +146,10 @@ function Picture(arrPixels) {
 
 Picture.prototype.clone = function() {
 	var output = [];
-	for(l = 0; l < this.image.length; l++) {
+	for(let l = 0; l < this.image.length; l++) {
 		output.push([]);
-		for(m = 0; m < this.image[l].length; m++) {
-			output[l].push(this.image[l][m].clone());
+		for(let m = 0; m < this.image[l].length; m++) {
+			output[l].push(this.image[l][m]);
 		}
 	}
 	return new Picture(output);
