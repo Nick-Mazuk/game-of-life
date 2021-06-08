0e945b58ae330e9f3b9f2d8c9ab9876cb9513821
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-06-08 11:47:03 -0700

    improve rendering performance 20x

--- src/lib/components/board-container.svelte
+++ src/lib/components/board-container.svelte
@@ -2,27 +2,82 @@
     import { browser } from '$app/env'
 
     import LoadingSpinner from '@nick-mazuk/ui-svelte/src/elements/loading-spinner/loading-spinner.svelte'
+    import { colorsHexMap } from '@nick-mazuk/ui-config/lib/colors.ts'
 
     import { BOARD_HEIGHT, BOARD_WIDTH } from '$lib/constants'
     import { board } from '$lib/stores/board'
-    import Square from '$lib/components/square.svelte'
+    import type { Board, SquareType } from '$lib/types'
+
+    type Context = CanvasRenderingContext2D | null | undefined
+
+    let canvas: HTMLCanvasElement | undefined
+    let canvasWidth: number | undefined
+    let canvasHeight: number | undefined
+    $: context = canvas?.getContext('2d')
+
+    const clearCanvas = (context: CanvasRenderingContext2D) => {
+        if (!canvas) return
+        context.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    const drawSquare = (x: number, y: number, width: number, height: number, type: SquareType) => {
+        if (!context || !canvas || type === 'dead') return
+        if (type === 'primary') {
+            context.fillStyle = colorsHexMap.primary
+        } else {
+            context.fillStyle = colorsHexMap.success
+        }
+        context.beginPath()
+        context.rect(x, y, width, height)
+        context.closePath()
+
+        context.fill()
+    }
+
+    const drawBoard = (context: Context, board: Board) => {
+        if (!context || !canvas || !canvasWidth || !canvasHeight) return
+        const { squares } = board
+        const squareWidth = canvasWidth / BOARD_WIDTH
+        const squareHeight = canvasHeight / BOARD_HEIGHT
+        clearCanvas(context)
+        for (let i = 0; i < squares.length; i++) {
+            for (let j = 0; j < squares[i].length; j++) {
+                drawSquare(
+                    i * squareWidth,
+                    j * squareHeight,
+                    squareWidth,
+                    squareHeight,
+                    squares[i][j]
+                )
+            }
+        }
+    }
+
+    $: if (!resized && context) {
+        context.scale(devicePixelRatio, devicePixelRatio)
+        console.log('resized')
+        resized = true
+    }
+    $: drawBoard(context, $board)
+    $: devicePixelRatio = typeof window === 'undefined' ? 1 : window.devicePixelRatio
+    $: if (canvas && canvasHeight && canvasWidth) {
+        canvas.width = canvasWidth * devicePixelRatio
+        canvas.height = canvasHeight * devicePixelRatio
+    }
+    let resized = false
+
 </script>
 
-{#if browser}
-    <div
-        class="grid gap-0.5"
-        style="{`grid-template-columns: repeat(${BOARD_WIDTH}, 1fr); grid-template-rows: repeat(${BOARD_HEIGHT}, 1fr);`}"
-    >
-        {#each $board.squares as row}
-            {#each row as square}
-                <Square type="{square}" />
-            {/each}
-        {/each}
-    </div>
-{:else}
-    <div class="aspect-w-3 aspect-h-2 relative">
+<div class="aspect-w-3 aspect-h-2 relative">
+    {#if browser}
+        <canvas
+            bind:this="{canvas}"
+            bind:clientWidth="{canvasWidth}"
+            bind:clientHeight="{canvasHeight}"
+            class="absolute inset-0"></canvas>
+    {:else}
         <div class="absolute inset-0 flex items-center justify-around">
             <LoadingSpinner />
         </div>
-    </div>
-{/if}
+    {/if}
+</div>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 import type { SquareType } from './types.d'
 
-export const BOARD_WIDTH = 50
+export const BOARD_WIDTH = 200
 export const BOARD_HEIGHT = Math.round((BOARD_WIDTH / 3) * 2)
 
 export const SQUARE_PROBABILITY: SquareType[] = ['dead', 'dead', 'dead', 'primary', 'success']
